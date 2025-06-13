c02069076db6516f2f75b57867d7d1cc39bc50d2
JustDamion <shirkeydamion@gmail.com>
2025-06-12 21:50:40 -0500

    Added resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,11 +6,12 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
+  name: 'Damion Shirkey',
   role: 'Quality Assurance Engineer',
   description:
     'As a dedicated Quality Assurance Engineer, I have honed my skills through hands-on experience in building robust and scalable automation scripts. My keen attention to detail and perfectionist mindset enable me to detect issues early, preventing small problems from becoming larger challenges. Passionate about continuous learning, I stay up to date with the fast-paced world of technology to ensure efficiency and innovation in my work. I thrive in collaborative environments and am committed to contributing to team success.',
-  resume: 'https://example.com',
+  resume:
+    'https://drive.google.com/uc?export=download&id=1oEE2TRTlf8ZkelBF2CYBCN2Mka0DVJfI',
   social: {
     linkedin: 'https://www.linkedin.com/in/damionshirkey/',
     github: 'https://github.com/JustDamion',
@@ -20,30 +21,30 @@ const about = {
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
-  {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
+  // {
+  //   name: 'Project 1',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 2',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 3',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
 ]
 
 const skills = [
